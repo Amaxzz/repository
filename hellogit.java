595a55c44863c3b57629011e1658d8e24d81d977
asdfning <413125687@qq.com>
2019-09-07 16:26:56 +0800

    11

--- hellogit.java
+++ hellogit.java
@@ -2,6 +2,6 @@ public class Hellogit{
   public static void main(String arg[]){
     System.out.println("hello java1");
     System.out.println("hello java2");
-    
+    System.out.println("sssss");
   }
 }
